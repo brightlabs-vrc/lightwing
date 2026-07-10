a17137abfcddcbc1ba48bf5e7a446184bbaf9e3f
Ayase Minori <chinodesuuu@gmail.com>
2026-07-10 01:29:48 +0000

    fuck you mean I have to do this
    Signed-off-by: GitHub <noreply@github.com>

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig, env } from "prisma/config";
 
+const databaseURL =
+  process.env.DATABASE_URL ??
+  "postgresql://postgres:postgres@localhost:5432/postgres";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL") ?? "postgresql://postgres:postgres@localhost:5432/postgres",
+    url: databaseURL || env("DATABASE_URL"),
   },
   migrations: {
     path: "prisma/migrations",
