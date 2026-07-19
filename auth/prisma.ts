1193291233714e517f9ebc70b57ce1079ccc1a6f
Ayase Minori <ayane@duck.com>
2026-07-20 05:47:12 +0800

    move database initialization at root

--- auth/prisma.ts
+++ auth/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-import { db } from "./db";
+import { db } from "../db";
 
 const adapter = new PrismaPg({
   connectionString: db.connectionString,
